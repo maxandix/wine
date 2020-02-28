d8655387660867f80ca333e72045222833ac5522
Бирюков Максим <maxandix@gmail.com>
2020-02-28 17:44:31 +0300

    fix comments

--- main.py
+++ main.py
@@ -2,32 +2,41 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime
 import pandas
+from collections import defaultdict
+from dotenv import load_dotenv
+import os
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('template.html')
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-drinks = pandas.read_excel('wine.xlsx', na_values=['N/A', 'NA'], keep_default_na=False).to_dict(orient='record')
+    template = env.get_template('template.html')
 
-categorized_drinks = {}
-for drink in drinks:
-    if drink['Категория'] not in categorized_drinks:
-        categorized_drinks[drink['Категория']] = []
-    categorized_drinks[drink['Категория']].append(drink)
+    load_dotenv()
+    drinks_file_path = os.getenv('WINE_DRINKS_FILE_PATH')
+    drinks = pandas.read_excel(drinks_file_path, na_values=['N/A', 'NA'], keep_default_na=False).to_dict(orient='record')
 
-year_of_foundation = 1920
-age = datetime.now().year - year_of_foundation
+    categorized_drinks = defaultdict(list)
+    for drink in drinks:
+        categorized_drinks[drink['Категория']].append(drink)
 
-rendered_page = template.render(
-    age=age,
-    categorized_drinks=categorized_drinks,
-)
+    year_of_foundation = 1920
+    age = datetime.now().year - year_of_foundation
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    rendered_page = template.render(
+        age=age,
+        categorized_drinks=categorized_drinks,
+    )
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
